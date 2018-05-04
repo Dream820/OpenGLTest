b9a213e18fdeea3b10f020c886dd7805f68495cb
nujjj <yanjun@weikuo.cc>
2018-05-04 15:08:22 +0800

    master

---
+++ app/src/main/java/com/comet/myopengl/MyApplication.java
@@ -0,0 +1,12 @@
+package com.comet.myopengl;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    public static MyApplication myApplication;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        myApplication = this;
+    }
+}
