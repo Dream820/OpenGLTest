7141c0956d6c2d2f9a5483da6606c86bd075cc04
nujjj <yanjun@weikuo.cc>
2018-05-07 13:40:49 +0800

    11

--- app/src/main/java/com/comet/myopengl/MyGLSurfaceView.java
+++ app/src/main/java/com/comet/myopengl/MyGLSurfaceView.java
@@ -29,29 +29,19 @@ public class MyGLSurfaceView extends GLSurfaceView {
     public boolean onTouchEvent(MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
-
-        // If a touch is moved on the screen
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            // Calculate the change
             float dx = x - oldX;
             float dy = y - oldY;
-            // Define an upper area of 10% on the screen
             int upperArea = 0;
 
-            // Zoom in/out if the touch move has been made in the upper
             if (y < upperArea) {
                 z -= dx * TOUCH_SCALE / 2;
-
-                // Rotate around the axis otherwise
             } else {
                 advanceRenderer.yrot += dx * TOUCH_SCALE;
             }
         }
-
-        // Remember the values
         oldX = x;
         oldY = y;
-
         requestRender();
         return true;
     }
