015c342c9de32e4ca61cfd6dfe25cbf1d6e05242
nujjj <yanjun@weikuo.cc>
2018-05-07 10:40:50 +0800

    11

--- app/src/main/java/com/comet/myopengl/sample2/AdvanceRenderer.java
+++ app/src/main/java/com/comet/myopengl/sample2/AdvanceRenderer.java
@@ -57,15 +57,12 @@ public class AdvanceRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        if (height == 0) {  // Prevent A Divide By Zero By
-            height = 1;     // Making Height Equal One
+        if (height == 0) {
+            height = 1;
         }
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
 
-        gl.glViewport(0, 0, width, height); // Reset The Current Viewport
-        gl.glMatrixMode(GL10.GL_PROJECTION);       // Select The Projection Matrix
-        gl.glLoadIdentity(); // Reset The Projection Matrix
-
-        // Calculate The Aspect Ratio Of The Window
         GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
         gl.glMatrixMode(GL10.GL_MODELVIEW); // Select The Modelview Matrix
         gl.glLoadIdentity(); // Reset The Modelview Matrix
@@ -75,7 +72,6 @@ public class AdvanceRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-//        gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
         GLU.gluLookAt(gl, 0.0f,
                 0.6f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1, 0f);
