19c9cba758edf542c07938ca6eadbd8aed5e1ce4
闫军 <yanjun@weikuo.cc>
2018-05-09 10:07:16 +0800

    master

--- app/src/main/java/com/comet/myopengl/zero/MyGLSurfaceView.java
+++ app/src/main/java/com/comet/myopengl/zero/MyGLSurfaceView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 
 public class MyGLSurfaceView extends GLSurfaceView {
@@ -29,7 +28,6 @@ public class MyGLSurfaceView extends GLSurfaceView {
             float x = event.getX();
             float y = event.getY();
             if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                Log.d("onTouchEventtt", advanceRenderer.yrot + "");
                 float dx = x - oldX;
                 if (y >= 0) {
                     advanceRenderer.yrot += dx * TOUCH_SCALE;
@@ -37,23 +35,20 @@ public class MyGLSurfaceView extends GLSurfaceView {
                 oldX = x;
             } else if (event.getAction() == MotionEvent.ACTION_UP) {
                 //抬起的时候需要计算 接近那个点
+                advanceRenderer.isCorrecting = true;
                 float ramain = advanceRenderer.yrot % 60;
                 if (ramain >= 0 && ramain < 30) {
                     advanceRenderer.plusOrMinus = -1;
                     advanceRenderer.time = (int) (ramain / 3f) + 1;
-                    advanceRenderer.isCorrecting = true;
                 } else if (ramain >= 30 && ramain <= 60f) {
                     advanceRenderer.plusOrMinus = 1;
                     advanceRenderer.time = (int) ((60f - ramain) / 3f) + 1;
-                    advanceRenderer.isCorrecting = true;
                 } else if (ramain <= 0 && ramain > -30) {
                     advanceRenderer.plusOrMinus = -1;
                     advanceRenderer.time = (int) (ramain / 3f) + 1;
-                    advanceRenderer.isCorrecting = true;
                 } else if (ramain <= -30 && ramain >= -60) {
                     advanceRenderer.plusOrMinus = 1;
                     advanceRenderer.time = (int) (-60f - ramain) / 3 + 1;
-                    advanceRenderer.isCorrecting = true;
                 }
             } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 oldX = x;
