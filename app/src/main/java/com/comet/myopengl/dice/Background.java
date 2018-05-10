9c80f8747549fe22459cdd84c97e635aa8194adc
闫军 <yanjun@weikuo.cc>
2018-05-10 11:00:07 +0800

    master

--- app/src/main/java/com/comet/myopengl/dice/Background.java
+++ app/src/main/java/com/comet/myopengl/dice/Background.java
@@ -105,13 +105,6 @@ public class Background {
         GLES20.glVertexAttribPointer(mTextureCoordinationHandle,2, GLES20.GL_FLOAT,false,2*4,mVerticesTextureBuffer);
         GLES20.glEnableVertexAttribArray(mTextureCoordinationHandle);
 
-        // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
-        // (which currently contains model * view).
-//        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
-        // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
-        // (which now contains model * view * projection).
-//        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
-
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureId);
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, MatrixState.getFinalMatrix(), 0);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
