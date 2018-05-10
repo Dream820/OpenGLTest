46f78c9f9d4b8af23a6771aedb6c98d2048ef0c3
闫军 <yanjun@weikuo.cc>
2018-05-10 17:11:33 +0800

    master

---
+++ app/src/main/java/com/comet/myopengl/gl2/CuneActivity.java
@@ -0,0 +1,19 @@
+package com.comet.myopengl.gl2;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+import com.comet.myopengl.zero.GLImage;
+
+public class CuneActivity extends Activity {
+    /** Called when the activity is first created. */  
+    @Override  
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GLImage.load(getResources());
+        GLSurfaceView   glView = new GLSurfaceView(this);
+        glView.setRenderer(new CubeRender());
+        setContentView(glView);
+    }  
+} 
