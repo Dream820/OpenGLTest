288afb53b0ee75b12cdb27342789b2735928fa8e
闫军 <yanjun@weikuo.cc>
2018-05-10 11:40:49 +0800

    master

--- app/src/main/java/com/comet/myopengl/simpleone/SimpleRender.java
+++ app/src/main/java/com/comet/myopengl/simpleone/SimpleRender.java
@@ -1,10 +1,14 @@
 package com.comet.myopengl.simpleone;
 
 import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
 import android.opengl.GLUtils;
 
 import com.comet.myopengl.zero.GLImage;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -34,6 +38,10 @@ public class SimpleRender implements GLSurfaceView.Renderer {
             0, 1, 3, 0, 3, 2
     };
 
+    private FloatBuffer vertexBuffer;
+    private FloatBuffer floatBuffer;
+    private ByteBuffer indexBuffer0;
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         gl.glEnable(GL10.GL_LIGHT0); // Enable Light 0
@@ -58,15 +66,60 @@ public class SimpleRender implements GLSurfaceView.Renderer {
         GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, GLImage.mBitmap1, 0);
         gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
         gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
+
+        ByteBuffer byteBuf
+                = ByteBuffer.allocateDirect(vertices.length * 4);
+        byteBuf.order(ByteOrder.nativeOrder());
+        vertexBuffer = byteBuf.asFloatBuffer();
+        vertexBuffer.put(vertices);
+        vertexBuffer.position(0);
+
+        byteBuf = ByteBuffer.allocateDirect(texture.length * 4);
+        byteBuf.order(ByteOrder.nativeOrder());
+        floatBuffer = byteBuf.asFloatBuffer();
+        floatBuffer.put(texture);
+        floatBuffer.position(0);
+
+        indexBuffer0 = ByteBuffer.allocateDirect(indices0.length);
+        indexBuffer0.put(indices0);
+        indexBuffer0.position(0);
+
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+        if (height == 0) {
+            height = 1;
+        }
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+        GLU.gluLookAt(gl, 0.0f,
+                0.6f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1, 0f);
+        gl.glEnable(GL10.GL_DEPTH_TEST); // 开启时时只绘制前面的一层
+        gl.glScalef(0.2f, 0.2f, 0.2f);
 
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+        gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
+
+        gl.glFrontFace(GL10.GL_CCW);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, floatBuffer);//设置纹理
+
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+        gl.glDrawElements(GL10.GL_TRIANGLES, indices0.length, GL10.GL_UNSIGNED_BYTE, indexBuffer0);
+
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+        gl.glDisableClientState(GL10.GL_NORMAL_ARRAY);
     }
 }
