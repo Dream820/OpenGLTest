d417e7a033678ea43346cc1538c749f44feb8bcd
闫军 <yanjun@weikuo.cc>
2018-05-11 10:48:33 +0800

    master

--- app/src/main/java/com/comet/myopengl/simpleone2/SimpleOneActivity2Side.java
+++ app/src/main/java/com/comet/myopengl/simpleone2/SimpleOneActivity2Side.java
@@ -166,11 +166,11 @@ public class SimpleOneActivity2Side extends Activity {
             GLES20.glVertexAttribPointer(mTexCoordHandle, 2, GLES20.GL_FLOAT, false, 0,
                     mTexVertexBuffer);
 
-            int[] texNames = new int[1];
-            GLES20.glGenTextures(1, texNames, 0);
-            mTexName = texNames[0];
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTexName);
+//            int[] texNames = new int[1];
+//            GLES20.glGenTextures(1, texNames, 0);
+//            mTexName = texNames[0];
+//            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+//            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTexName);
             GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER,
                     GLES20.GL_LINEAR);
             GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER,
