a774ee839de036d6f7e80b7d69368ceb759bf05e
闫军 <yanjun@weikuo.cc>
2018-05-15 11:43:42 +0800

    master

--- app/src/main/java/com/comet/myopengl/study/AirHook2Renderer.java
+++ app/src/main/java/com/comet/myopengl/study/AirHook2Renderer.java
@@ -15,22 +15,36 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class AirHook2Renderer implements GLSurfaceView.Renderer {
-    private static final int POSITION_COMOPNENT_COUNT = 2;//数量 3 xyz 4xyzc
     private static final int BYTES_PER_FLOAT = 4;
-    private float[] tableVertices = {
-            0,     0,
-            -0.5f, -0.5f,
-            0.5f, -0.5f,
-            0.5f,  0.5f,
-            -0.5f,  0.5f,
-            -0.5f, -0.5f,
-    };
     private FloatBuffer vertexData;
+    private static final int POSITION_COMOPNENT_COUNT = 2;//数量 3 xyz 4xyzc
+    private float[] tableVertices = {
+            // Triangle Fan
+            0f, 0f, 1f, 1f, 1f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+
+            // Line 1
+            -0.5f, 0f, 1f, 0f, 0f,
+            0.5f, 0f, 1f, 0f, 0f,
+
+            // Mallets
+            0f, -0.25f, 0f, 0f, 1f,
+            0f, 0.25f, 1f, 0f, 0f
+    };
+
     private static final String A_POSITION = "a_Position";
     private int aPostionLocation;
-    private int uColorLocation;
 
-    private static final String U_COLOR = "u_Color";
+
+    private static final String A_COLOR = "a_Color";
+    private static final int COLOR_COMPNENT_COUNT = 3;
+    private static final int STRIDE = (POSITION_COMOPNENT_COUNT + COLOR_COMPNENT_COUNT) * BYTES_PER_FLOAT;
+    private int aColorLocation;
+
     public AirHook2Renderer() {
         vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT)
                 .order(ByteOrder.nativeOrder())
@@ -54,7 +68,7 @@ public class AirHook2Renderer implements GLSurfaceView.Renderer {
 
         GLES20.glUseProgram(program);//使用工程
 
-        uColorLocation = GLES20.glGetUniformLocation(program, U_COLOR);//返回着色器参数 在缓冲区中的位置
+        aColorLocation = GLES20.glGetUniformLocation(program, A_COLOR);//返回着色器参数 在缓冲区中的位置
         aPostionLocation = GLES20.glGetAttribLocation(program, A_POSITION);//返回着色器参数 在缓冲区中的位置
 
         vertexData.position(0);
@@ -65,6 +79,12 @@ public class AirHook2Renderer implements GLSurfaceView.Renderer {
         GLES20.glEnableVertexAttribArray(
                 aPostionLocation);// Enable or disable a generic vertex attribute array
 
+
+        vertexData.position(POSITION_COMOPNENT_COUNT);
+        GLES20.glVertexAttribPointer(aColorLocation,
+                COLOR_COMPNENT_COUNT, GLES20.GL_FLOAT,
+                false, STRIDE, vertexData);
+        GLES20.glEnableVertexAttribArray(aColorLocation);
         Log.d("GLES20", "11111");
     }
 
